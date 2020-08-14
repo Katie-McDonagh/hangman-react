6b08b1d3193e3c0f22baf26b1280a482cc570c7d
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-14 15:49:12 +0100

    fix logic in setting wrongletters

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
 
 
+
+
 import './App.css';
 
 const words = ['application', 'programming', 'interface', 'wizard'];
-
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
-  const [wrongLetters, setwrongLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+
+  useEffect(() => {
+  const handleKeyDown = event => {
+    const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            // showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(wrongLetters => [...wrongLetters, letter]);
+            } else {
+            // showNotification();
+          }
+        }
+      }
+  }
+
+  window.addEventListener('keydown', handleKeyDown);
+  // below function called to clean up after event listener, so we only have one event listener happening at once.
+  return () => window.removeEventListener('keydown', handleKeyDown);
+  // anytimes the below gets updated it will call the function
+}, [correctLetters, wrongLetters, playable]);
 
   return (
     <>
     <Header />
     <div className="game-container">
       <Figure />
-      <WrongLetters />
+      <WrongLetters wrongLetters={wrongLetters} />
       <Word selectedWord={selectedWord} correctLetters={correctLetters} />
     </div>
     </>
