67af6e916165c4828d422fcb8b2e443cb1c25bef
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-14 13:23:54 +0100

    WrongLetters component built

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+
 
 import './App.css';
 
@@ -8,6 +10,10 @@ function App() {
   return (
     <>
     <Header />
+    <div className="game-container">
+      <Figure />
+      <WrongLetters />
+    </div>
     </>
   );
 }
---
+++ src/components/WrongLetters.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const wrongLetters = () => {
+  return (
+    <div className="wrong-letters-container">
+    <div id="wrong-letters"></div>
+  </div>
+  )
+}
+
+export default wrongLetters
