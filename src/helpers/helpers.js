3fa95122ed5eb20454825b945b2891d8d3bb1076
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-14 21:15:22 +0100

    pop up conponent for win/lose

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -4,3 +4,21 @@ export function showNotification(setter) {
     setter(false);
   }, 2000);
 }
+
+export function checkWin(correct, wrong, word){
+  let status = 'win';
+
+  // check for a win
+
+  word.split('').forEach(letter => {
+    if(!correct.includes(letter)){
+      status = '';
+    }
+  })
+
+  // check for loss
+
+  if(wrong.length === 6) status = 'lose';
+
+  return status;
+}
