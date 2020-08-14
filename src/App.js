c791cd3965a5151b2cb08ea327d7711bbf3d5895
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-14 14:30:27 +0100

    useState set up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
@@ -7,7 +7,15 @@ import Word from './components/Word';
 
 import './App.css';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setwrongLetters] = useState([]);
+
   return (
     <>
     <Header />
