ecabaae27adfdbb4841938deddfe194724796c87
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-14 13:03:04 +0100

    add component -header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    //the empty tags are called a fragment 
+    <>
+      <h1>Hangman</h1>
+      <p>Find the hidden word - Enter a letter</p>
+    </>
+  )
+}
+
+export default Header
+
+
+
